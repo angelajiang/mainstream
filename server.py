5a7e7fb1e03cc0001405faf3ac5ebe245c4cfcc2
Angela Jiang <to.ajiang@gmail.com>
2017-05-16 15:16:14 -0400

    Refactored server to separate helper and exposed funcs

--- server.py
+++ server.py
@@ -14,8 +14,7 @@ import Pyro4
 APPS = {}
 MAX_LAYERS = 314
 
-@Pyro4.expose
-class Trainer(object):
+class Helpers():
 
     def __init__(self):
         global APPS
@@ -23,22 +22,7 @@ class Trainer(object):
         self._apps = APPS
         self._max_layers = MAX_LAYERS
 
-    def schedule(self):
-        if len(self._apps) == 0:
-            return {}
-
-        all_min_layers_trained = []
-        for uuid, app_data in self._apps.iteritems():
-            max_acc = max(app_data["accuracy"].values())
-            acc_threshold = max_acc - (app_data["acc_dev_percent"] * max_acc)
-            min_layers_trained = min([layers_trained for layers_trained, accuracy in app_data["accuracy"].iteritems()  \
-                                                        if accuracy >= acc_threshold])
-            all_min_layers_trained.append(min_layers_trained)
-        shared_layer = self._max_layers - max(all_min_layers_trained)
-
-        return self.get_classifier_json(shared_layer)
-
-    def get_accuracy_per_layer(self, image_dir, config_file, max_layers, layers_stride, patience, mock=False):
+    def get_accuracy_per_layer(self, uuid, image_dir, config_file, max_layers, layers_stride, patience, mock=False):
         for num_training_layers in range(0, max_layers, layers_stride):
             num_training_layers = max(1, num_training_layers)
             if mock:
@@ -47,7 +31,7 @@ class Trainer(object):
                 print "[server] ================= Finetunning", num_training_layers, "layers ================= "
                 ft_obj = ft.FineTunerFast(config_file, image_dir, "/tmp/history", patience)
                 acc = ft_obj.finetune(num_training_layers)
-            self._apps[self._app_uuid]["accuracy"][num_training_layers] = acc
+            self._apps[uuid]["accuracy"][num_training_layers] = acc
 
     def get_classifier_json(self, shared_layer):
         pipeline_json = copy.deepcopy(BASIC_PIPELINE)
@@ -73,6 +57,31 @@ class Trainer(object):
 
         return pipeline_json
 
+@Pyro4.expose
+class Trainer(object):
+
+    def __init__(self):
+        global APPS
+        global MAX_LAYERS
+        self._apps = APPS
+        self._max_layers = MAX_LAYERS
+        self.helpers = Helpers()
+
+    def schedule(self):
+        if len(self._apps) == 0:
+            return {}
+
+        all_min_layers_trained = []
+        for uuid, app_data in self._apps.iteritems():
+            max_acc = max(app_data["accuracy"].values())
+            acc_threshold = max_acc - (app_data["acc_dev_percent"] * max_acc)
+            min_layers_trained = min([layers_trained for layers_trained, accuracy in app_data["accuracy"].iteritems()  \
+                                                        if accuracy >= acc_threshold])
+            all_min_layers_trained.append(min_layers_trained)
+        shared_layer = self._max_layers - max(all_min_layers_trained)
+
+        return self.helpers.get_classifier_json(shared_layer)
+
     def list_apps(self):
         return self._apps
 
@@ -82,7 +91,7 @@ class Trainer(object):
         self._apps[self._app_uuid] = {"accuracy": {}, "acc_dev_percent": acc_dev_percent, "name": name}
 
         # Train app with different numbers of layers frozen
-        self.get_accuracy_per_layer(image_dir, config_file, self._max_layers, 50, 0, False)
+        self.helpers.get_accuracy_per_layer(self._app_uuid, image_dir, config_file, self._max_layers, 50, 0, True)
 
         return self._app_uuid
 
