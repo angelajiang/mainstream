f8ec3c4ed04ecf136b2b5fcc66f860d6a12e0687
Angela Jiang <to.ajiang@gmail.com>
2017-07-10 09:15:16 -0600

    Small pep8 changes

--- src/inference/inference_h5.py
+++ src/inference/inference_h5.py
@@ -10,6 +10,7 @@ import pprint as pp
 import tensorflow as tf
 import numpy as np
 
+
 def predict(model_path, dataset_dir):
     model, tags = net.load(model_path)
     data_X, data_y, tags = dataset.dataset(dataset_dir, 299)
@@ -26,4 +27,3 @@ if __name__ == "__main__":
     model_path_h5, dataset_dir = sys.argv[1:]
     predictions = predict(model_path_h5, dataset_dir)
     print predictions
-
