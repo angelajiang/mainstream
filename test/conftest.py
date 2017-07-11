e968eb24c75c599bd01d28b45753e87813285221
Angela Jiang <to.ajiang@gmail.com>
2017-07-10 19:05:00 -0600

    Add printing of test outcome

--- test/conftest.py
+++ test/conftest.py
@@ -1,6 +1,7 @@
 import os
 import pytest
 import shutil
+from _pytest.runner import runtestprotocol
 
 def pytest_addoption(parser):
     parser.addoption("--data_dir", action="store", default="~/")
@@ -11,6 +12,13 @@ def pytest_generate_tests(metafunc):
     data_val = metafunc.config.option.data_dir
     if 'data_dir' in metafunc.fixturenames and data_val is not None:
         metafunc.parametrize("data_dir", [data_val], scope="session")
+
+def pytest_runtest_protocol(item, nextitem):
+    reports = runtestprotocol(item, nextitem=nextitem)
+    for report in reports:
+        if report.when == 'call':
+            print '\n%s --- %s' % (item.name, report.outcome)
+    return True
 
 # Make temporary test directory
 @pytest.fixture(scope="session")
