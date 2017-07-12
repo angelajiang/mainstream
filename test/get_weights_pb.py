1419b4e39b659b0858fb27f8304c9395bdd14b7d
Angela Jiang <to.ajiang@gmail.com>
2017-07-12 10:33:04 -0400

    Clean pb get_weights

--- test/get_weights_pb.py
+++ test/get_weights_pb.py
@@ -5,58 +5,18 @@ import pprint as pp
 import tensorflow as tf
 
 
-def load_graph(frozen_graph_filename):
-    # We load the protobuf file from the disk and parse it to retrieve the
-    # unserialized graph_def
-    with tf.gfile.GFile(frozen_graph_filename, "rb") as f:
-        graph_def = tf.GraphDef()
-        graph_def.ParseFromString(f.read())
-
-    # Then, we can use again a convenient built-in function to import a
-    # graph_def into the current default Graph
-    with tf.Graph().as_default() as graph:
-        tf.import_graph_def(
-            graph_def, 
-            input_map=None, 
-            return_elements=None, 
-            name="prefix", 
-            op_dict=None, 
-            producer_op_list=None
-        )
-
-    input_name = graph.get_operations()[0].name+':0'
-    output_name = graph.get_operations()[-1].name+':0'
-
-    return graph, input_name, output_name
-
 def get_weights(model_path):
-    '''
-    # load tf graph
-    tf_model, tf_input, tf_output = load_graph(model_path)
 
-    # Create tensors for model input and output
-    x = tf_model.get_tensor_by_name(tf_input)
-    y = tf_model.get_tensor_by_name(tf_output)
-
-
-    with tf.Session(graph=tf_model) as sess:
-        graph_def = sess.graph.as_graph_def()
-        #print [n.name for n in tf.get_default_graph().as_graph_def().node]
-        op = tf.get_default_graph().get_operation_by_name("prefix/conv2d_1/kernel")
-        print op.values
-        '''
-
+    model_ckpt_path = model_path + ".ckpt"
     with tf.Session() as sess:
-        new_saver = tf.train.import_meta_graph(model_path + ".meta")
-        new_saver.restore(sess, model_path)
+        new_saver = tf.train.import_meta_graph(model_ckpt_path + ".meta")
+        new_saver.restore(sess, model_ckpt_path)
         variables_names = [v.name for v in tf.trainable_variables()]
         values = sess.run(variables_names)
+        all_weights = {}
         for k, v in zip(variables_names, values):
-            print "----------------", k, "----------------"
-            print "Shape: ", v.shape
-            print v
-
-    return {}
+            all_weights[k] = v
+    return all_weights
 
 if __name__ == "__main__":
     model_path_pb = sys.argv[1]
