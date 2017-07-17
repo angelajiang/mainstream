c221ab0619e4f48baceafbc62f2ffa88d5d0d7b0
Angela Jiang <to.ajiang@gmail.com>
2017-07-17 17:43:53 -0600

    Add test dataset

---
+++ src/util/test_dataset.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.append('src/util')
+import dataset
+import scipy.misc
+
+
+def run(dataset_dir, output_dir):
+
+    # Get data
+    data_X, data_y, _ = dataset.dataset(dataset_dir, 299)
+    for i, x in enumerate(data_X):
+        output_file = output_dir + "/frame-" + str(i) + ".png"
+        scipy.misc.imsave(output_file, x)
+
+if __name__ == "__main__":
+    dataset_dir, output_dir = sys.argv[1:]
+    run(dataset_dir, output_dir)
