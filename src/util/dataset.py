ebf24cae9d6d9f5366f84914b8fc55c996f57fba
Angela Jiang <to.ajiang@gmail.com>
2017-07-17 20:00:57 -0400

    Add option to remove randomness

--- src/util/dataset.py
+++ src/util/dataset.py
@@ -19,7 +19,7 @@ def reverse_preprocess_input(x0):
     return x
 
 
-def dataset(base_dir, n):
+def dataset(base_dir, n, random=True):
     d = defaultdict(list)
     for root, subdirs, files in os.walk(base_dir):
         for filename in files:
@@ -67,9 +67,10 @@ def dataset(base_dir, n):
     X = preprocess_input(X)
     y = np.array(y)
 
-    perm = np.random.permutation(len(y))
-    X = X[perm]
-    y = y[perm]
+    if (random):
+        perm = np.random.permutation(len(y))
+        X = X[perm]
+        y = y[perm]
 
     print "classes:"
     for class_index, class_name in enumerate(tags):
--- src/util/test_dataset.py
+++ src/util/test_dataset.py
@@ -7,7 +7,7 @@ import scipy.misc
 def run(dataset_dir, output_dir):
 
     # Get data
-    data_X, data_y, _ = dataset.dataset(dataset_dir, 299)
+    data_X, data_y, _ = dataset.dataset(dataset_dir, 299, False)
     for i, x in enumerate(data_X):
         output_file = output_dir + "/frame-" + str(i) + ".png"
         scipy.misc.imsave(output_file, x)
