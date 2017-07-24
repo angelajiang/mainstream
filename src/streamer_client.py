ece2acdb10193b9ba24433e6f93335c0817af089
Angela Jiang <to.ajiang@gmail.com>
2017-07-24 19:49:19 -0400

    Add sharing and not sharing option to streamer client

--- src/streamer_client.py
+++ src/streamer_client.py
@@ -1,6 +1,7 @@
 import zmq
+import sys
 
-import zmq
+shared = int(sys.argv[1])
 
 context = zmq.Context()
 
@@ -11,7 +12,53 @@ socket.connect("tcp://localhost:5555")
 
 print "Sending request"
 
-json_obj = [{"name": "baseNN"}]
+if shared:
+    print "Sharing."
+    json_obj = {"nnes": [{"name": "baseNN",
+                          "model_path": "/home/ahjiang/models/flowers-40-0.0001-310-frozen.pb",
+                          "input_layer": "input_1",
+                          "output_layer": "mixed7/concat",
+                          "channels": 3,
+                          "width": 299,
+                          "height": 299,
+                          "input_processor": ""},
+                         {"name": "task1",
+                          "model_path": "/home/ahjiang/models/trains-40-0.0001-310-frozen.pb",
+                          "input_layer": "mixed7/concat",
+                          "output_layer": "dense_2/Softmax:0",
+                          "channels": 1,
+                          "width": 1,
+                          "height": 1,
+                          "input_processor": "baseNN"},
+                         {"name": "task2",
+                          "model_path": "/home/ahjiang/models/flowers-40-0.0001-310-frozen.pb",
+                          "input_layer": "mixed7/concat",
+                          "output_layer": "dense_2/Softmax:0",
+                          "channels": 1,
+                          "width": 1,
+                          "height": 1,
+                          "input_processor": "baseNN"}
+                         ]}
+else:
+    print "Not sharing."
+    json_obj = {"nnes": [{"name": "task1",
+                          "model_path": "/home/ahjiang/models/flowers-40-0.0001-310-frozen.pb",
+                          "input_layer": "input_1",
+                          "output_layer": "dense_2/Softmax:0",
+                          "channels": 3,
+                          "width": 299,
+                          "height": 299,
+                          "input_processor": ""},
+                         {"name": "task2",
+                          "model_path": "/home/ahjiang/models/trains-40-0.0001-310-frozen.pb",
+                          "input_layer": "input_1",
+                          "output_layer": "dense_2/Softmax:0",
+                          "channels": 1,
+                          "width": 1,
+                          "height": 1,
+                          "input_processor": ""}
+                         ]}
+
 socket.send_json(json_obj)
 
 # Get the reply.
