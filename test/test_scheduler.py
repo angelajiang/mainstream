07731a6d6ae122a394c0a8e28b85140021ab5501
Angela Jiang <to.ajiang@gmail.com>
2017-07-25 01:09:07 -0400

    Add test for share everything

--- test/test_scheduler.py
+++ test/test_scheduler.py
@@ -4,52 +4,100 @@ import scheduler
 import pprint as pp
 import pytest
 
+model_desc = {"total_layers": 40,
+              "channels": 3,
+              "height": 299,
+              "width": 299,
+              "frozen_layer_names": {1: "input",
+                                     10: "conv1",
+                                     20: "conv2",
+                                     30: "pool",
+                                     40: "softmax"}}
+
+app1 = {"model_path": "app1_model.pb",
+        "accuracies": {1: 1,
+                       10: 0.8,
+                       20: 0.6,
+                       30: 0.6,
+                       40: 0.2
+                      }
+        }
+app2 = {"model_path": "app2_model.pb",
+        "accuracies": {1: 1,
+                       10: 1,
+                       20: 0.8,
+                       30: 0.6,
+                       40: 0.2
+                      }
+        }
+app3 = {"model_path": "app3_model.pb",
+        "accuracies": {1: 1,
+                       10: 1,
+                       20: 1,
+                       30: 0.8,
+                       40: 0.6
+                      }
+        }
+app4 = {"model_path": "app4_model.pb",
+        "accuracies": {1: 1,
+                       10: 1,
+                       20: 1,
+                       30: 0.8,
+                       40: 0.6
+                      }
+        }
+
+@pytest.mark.unit
+def test_share_everything():
+
+    apps = [app1, app2]
+    threshold = 1
+
+    ref_num_frozen = [40, 40]
+    ref_schedule = \
+            [{"net_id": 0,
+              "parent_id": -1,
+              "input_layer": "input",
+              "output_layer": "pool",
+              "channels": 3,
+              "height": 299,
+              "width": 299,
+              "model_path": "app1_model.pb"
+             },
+             {"net_id": 1,
+              "parent_id": 0,
+              "input_layer": "pool",
+              "output_layer": "softmax",
+              "channels": 3,
+              "height": 299,
+              "width": 299,
+              "model_path": "app1_model.pb"
+              },
+             {"net_id": 2,
+              "parent_id": 0,
+              "input_layer": "pool",
+              "output_layer": "softmax",
+              "channels": 3,
+              "height": 299,
+              "width": 299,
+              "model_path": "app2_model.pb"
+              }]
+
+    # Test get_num_frozen is accurate
+    for app, ref in zip(apps, ref_num_frozen):
+        accs = app["accuracies"]
+        num_frozen = scheduler.get_num_frozen(accs, threshold)
+        assert num_frozen == ref
+
+    # Test scheduler is accurate
+    schedule = scheduler.schedule(apps, threshold, model_desc)
+    pp.pprint(schedule)
+    assert ref_schedule ==  schedule
+
 @pytest.mark.unit
 def test_scheduler():
     # Set up data
 
-    model_desc = {"total_layers": 40,
-                  "channels": 3,
-                  "height": 299,
-                  "width": 299,
-                  "frozen_layer_names": {1: "input",
-                                         10: "conv1",
-                                         20: "conv2",
-                                         30: "pool",
-                                         40: "softmax"}}
-
-    app1 = {"model_path": "app1_model.pb",
-            "accuracies": {1: 1,
-                           10: 0.8,
-                           20: 0.6,
-                           30: 0.6,
-                           40: 0.2
-                          }
-            }
-    app2 = {"model_path": "app2_model.pb",
-            "accuracies": {1: 1,
-                           10: 1,
-                           20: 0.8,
-                           30: 0.6,
-                           40: 0.2
-                          }
-            }
-    app3 = {"model_path": "app3_model.pb",
-            "accuracies": {1: 1,
-                           10: 1,
-                           20: 1,
-                           30: 0.8,
-                           40: 0.6
-                          }
-            }
-    app4 = {"model_path": "app4_model.pb",
-            "accuracies": {1: 1,
-                           10: 1,
-                           20: 1,
-                           30: 0.8,
-                           40: 0.6
-                          }
-            }
     apps = [app1, app2, app3, app4]
     threshold = 0.2
 
