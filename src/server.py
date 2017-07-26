0f9ad91d0ee3c16025fea95d57aefffd64c2e2bb
Angela Jiang <to.ajiang@gmail.com>
2017-07-26 09:30:18 -0600

    Remove scheduler, remove max_layers

--- src/server.py
+++ src/server.py
@@ -16,7 +16,6 @@ if len(sys.argv) < 2:
     sys.exit(-1)
 
 APPS = {}
-MAX_LAYERS = 314
 DB = redis.StrictRedis(host="localhost", port=int(sys.argv[1]), db=0)
 STORE = persistence.Persistence(DB)
 
@@ -56,32 +55,6 @@ class Helpers():
             acc = ft_obj.finetune(num_frozen_layers)
         return acc
 
-    def get_classifier_json(self, shared_layer, max_layers):
-        pipeline_json = copy.deepcopy(BASIC_PIPELINE)
-
-        base_nn = copy.deepcopy(BASIC_NN)
-        base_nn["parameters"]["start_layer"] = 1
-        base_nn["parameters"]["end_layer"] = shared_layer
-        base_nn["processor_name"] = "base_nn"
-        base_nn["inputs"]["input"] = "Transformer:output"
-        pipeline_json["processors"].append(base_nn)
-
-        app_uuids = self._store.get_app_uuids()
-        for uuid in app_uuids:
-            app_data = self._store.get_app_data(uuid)
-            task_nn = copy.deepcopy(BASIC_NN)
-            task_nn["processor_name"] = app_data["name"] + "_" + uuid + "_nn"
-            task_nn["parameters"]["start_layer"] = shared_layer
-            task_nn["parameters"]["end_layer"] = max_layers
-            task_nn["inputs"]["input"] = base_nn["processor_name"] + ":output"
-            pipeline_json["processors"].append(task_nn)
-            task_classifier = copy.deepcopy(BASIC_CLASSIFIER)
-            task_classifier["processor_name"] = app_data["name"] + "_" + uuid + "_classifier"
-            task_classifier["inputs"]["input"] = task_nn["processor_name"] + ":output"
-            pipeline_json["processors"].append(task_classifier)
-
-        return pipeline_json
-
 @Pyro4.expose
 class Trainer(object):
 
@@ -89,31 +62,10 @@ class Trainer(object):
         global MAX_LAYERS
         global STORE
         global CHOKEPOINTS
-        self._max_layers = MAX_LAYERS
         self._store = STORE
         self._chokepoints = CHOKEPOINTS
 
         self._helpers = Helpers(self._store)
-
-    def schedule(self):
-        # REDIS
-        app_uuids = self._store.get_app_uuids()
-        if len(app_uuids) == 0:
-            return {}
-
-        all_min_layers_trained = []
-        app_accuracies = self._store.get_apps_accuracies()
-        for uuid in app_uuids:
-            max_acc_layer = max(app_accuracies[uuid].keys())
-            max_acc = app_accuracies[uuid][max_acc_layer]
-            acc_dev_percent = self._store.get_acc_dev_by_app_uuid(uuid)
-            acc_threshold = max_acc - (acc_dev_percent * max_acc)
-            for app_uuid, accuracies in app_accuracies.iteritems():
-                min_layers_trained = min([layers_trained for layers_trained, accuracy in accuracies.iteritems()  \
-                                                            if accuracy >= acc_threshold])
-            all_min_layers_trained.append(min_layers_trained)
-        shared_layer = self._max_layers - max(all_min_layers_trained)
-        return self._helpers.get_classifier_json(shared_layer, self._max_layers)
 
     def list_apps(self):
         return self._store.get_apps_accuracies()
