70a9800afbde0686fb736c9b8aa6d8c9bf0786a1
Angela Jiang <to.ajiang@gmail.com>
2017-08-30 11:37:18 -0400

    Add tradeoff scheduler skeleton

---
+++ src/scheduler/tradeoff_scheduler.py
@@ -0,0 +1,34 @@
+import sys
+sys.path.append('src/scheduler')
+import scheduler
+import numpy as np
+import zmq
+
+
+def run(apps, model_desc, min_fps):
+
+    num_trials = 1
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://localhost:5555")
+
+    # Iterate until we converge to min_fps
+    print "Num apps:", len(apps), " Min FPS:", min_fps
+    sched = scheduler.schedule_no_sharing(apps, model_desc)
+
+    num_frozen_list = []
+
+    # Deploy schedule
+    fpses = []
+    for i in range(num_trials):
+        socket.send_json(sched)
+        fps_message = socket.recv()
+        fpses.append(float(fps_message))
+    avg_fps = np.average(fpses)
+    stdev = np.std(fpses)
+
+    #rel_accs = [get_relative_accuracy(app, num_frozen) \
+    #                for app, num_frozen in zip(apps, num_frozen_list)]
+    rel_accs = [1 for app in apps]
+
+    return np.average(rel_accs), num_frozen_list
