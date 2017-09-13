825f986ac680e7ba0b669d8bbaaa505e7377095c
Angela Jiang <to.ajiang@gmail.com>
2017-09-13 09:59:36 -0400

    Merged changes

--- src/scheduler/Scheduler.py
+++ src/scheduler/Scheduler.py
@@ -392,6 +392,14 @@ class Scheduler:
                                               app["event_length_ms"],
                                               self.stream_fps,
                                               observed_fps)
+            false_neg_rate2 = scheduler_util.get_false_neg_rate(
+                                              self.acc_dists[accuracy],
+                                              app["event_length_ms"],
+                                              self.stream_fps,
+                                              observed_fps)
+
+            print "COMPARE:", false_neg_rate, false_neg_rate2
+
             metrics.append(false_neg_rate)
 
             observed_unit = Schedule.ScheduleUnit(app,
