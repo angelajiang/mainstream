e5d525caf83301e3f3673eae288350a067af685d
Daniel Wong <wonglkd@gmail.com>
2017-09-19 20:56:29 -0400

    Add make_monotonic and combine_accs

--- data/app_data.py
+++ data/app_data.py
@@ -6,61 +6,87 @@ def flip(acc_map, total_layers):
         new_acc_map[key] = acc
     return new_acc_map
 
+
+def make_monotonic(acc_map, del_dup=True):
+    acc = reversed(sorted(acc_map.items()))
+    best_so_far = -1.
+    acc_map_ret = dict(acc_map)
+    for k, v in acc:
+        if v >= best_so_far:
+            best_so_far = v
+        else:
+            if del_dup:
+                del acc_map_ret[k]
+            else:
+                acc_map_ret[k] = best_so_far
+    return acc_map_ret
+
+
+def combine_accs(apps):
+    def flatten(lst):
+        return [l for sl in lst for l in sl]
+    layers = set(flatten([make_monotonic(a["accuracies"]).keys() for a in apps]))
+    for app in apps:
+        orig = make_monotonic(app["accuracies"], del_dup=False)
+        app["accuracies"] = {l: orig[l] for l in layers}
+    return apps
+
+
 # TODO: Get values from redis
-accuracy_flowers_inception = {#0:0.882,
-                              #4:0.882,
-                              #7:0.8834,
-                              #10:0.882,
-                              #11:0.8807,
+accuracy_flowers_inception = {0:0.882,
+                              4:0.882,
+                              7:0.8834,
+                              10:0.882,
+                              11:0.8807,
                               14:0.8834,
-                              #17:0.8807,
+                              17:0.8807,
                               18:0.882,
-                              #41:0.8807,
-                              #64:0.8807,
+                              41:0.8807,
+                              64:0.8807,
                               87:0.8807,
-                              #101:0.8765,
+                              101:0.8765,
                               133:0.8779,
                               165:0.8765,
                               197:0.8697,
-                              #229:0.8615,
+                              229:0.8615,
                               249:0.8477,
                               280:0.7284,
                               311:0.2606}
 
-accuracy_cats_inception = {#0:0.7684,
-                           #4:0.7720,
-                           #7:0.7720,
-                           #10:0.7708,
-                           #11:0.7809,
-                           #14:0.7858,
-                           #17:0.7834,
+accuracy_cats_inception = {0:0.7684,
+                           4:0.7720,
+                           7:0.7720,
+                           10:0.7708,
+                           11:0.7809,
+                           14:0.7858,
+                           17:0.7834,
                            18:0.7996,
                            41:0.7984,
-                           #64:0.7884,
-                           #87:0.7821,
-                           #101:0.7821,
+                           64:0.7884,
+                           87:0.7821,
+                           101:0.7821,
                            133:0.7922,
-                           #165:0.7821,
+                           165:0.7821,
                            197:0.7834,
                            229:0.7809,
                            249:0.7632,
                            280:0.7418,
                            311:0.4836}
 
-accuracy_trains_inception = flip({#314:0.9855,
-                            #310:0.9855,
-                            #307:0.9831,
-                            #304:0.9879,
-                            #303:0.9879,
-                            #300:0.9879,
-                            #297:0.9831,
-                            #296:0.9831,
-                            #273:0.9831,
-                            #250:0.9831,
-                            #227:0.9831,
-                            #213:0.9831,
-                            #181:0.9831,
-                            #149:0.9855,
+accuracy_trains_inception = flip({314:0.9855,
+                            310:0.9855,
+                            307:0.9831,
+                            304:0.9879,
+                            303:0.9879,
+                            300:0.9879,
+                            297:0.9831,
+                            296:0.9831,
+                            273:0.9831,
+                            250:0.9831,
+                            227:0.9831,
+                            213:0.9831,
+                            181:0.9831,
+                            149:0.9855,
                             117:0.9952,
                             85:0.9952,
                             65:0.9952,
@@ -69,18 +95,18 @@ accuracy_trains_inception = flip({#314:0.9855,
                             #1:0.6610
                             }, 314)
 
-accuracy_paris_inception = {#0:0.8922,
-                            #4:0.8922,
-                            #7:0.8892,
-                            #10:0.8922,
-                            #11:0.8922,
-                            #14:0.8922,
-                            #17:0.8922,
-                            #18:0.8922,
-                            #41:0.8922,
-                            #64:0.8922,
-                            #87:0.8922,
-                            #101:0.8922,
+accuracy_paris_inception = {0:0.8922,
+                            4:0.8922,
+                            7:0.8892,
+                            10:0.8922,
+                            11:0.8922,
+                            14:0.8922,
+                            17:0.8922,
+                            18:0.8922,
+                            41:0.8922,
+                            64:0.8922,
+                            87:0.8922,
+                            101:0.8922,
                             133:0.8952,
                             165:0.8832,
                             197:0.8772,
@@ -127,7 +153,7 @@ inception_layer_latencies =  [0.179, 0.179, 0.179, 0.179, 0.3691, 0.3691,
         0.0509, 0.0509]
 
 
-app_options = [
+app_options = combine_accs([
                {"accuracies": accuracy_flowers_inception,
                 "event_length_ms": 250,
                 "model_path": {
@@ -150,13 +176,14 @@ app_options = [
                     249:"flowers-310-frozen.pb",
                     280:"flowers-310-frozen.pb",
                     311:"flowers-310-frozen.pb"}
-                }
-               #{"accuracies": accuracy_cats_inception,
-               # "event_length_ms": 250,
-               # "model_path": "flowers-310-frozen.pb"}
-               #{"accuracies": accuracy_paris_inception,
-               # "model_path": "paris-95-frozen.pb"}
-               ]
+                },
+               {"accuracies": accuracy_cats_inception,
+               "event_length_ms": 250,
+               "model_path": "flowers-310-frozen.pb"},
+               {"accuracies": accuracy_paris_inception,
+               "model_path": "paris-95-frozen.pb",
+               "event_length_ms": 250},
+               ])
 
 model_desc = {"total_layers": 314,
               "channels": 1,
