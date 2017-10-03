8fb3443af814b040b1b4b6f052eee89f4a5d21a7
Angela Jiang <to.ajiang@gmail.com>
2017-10-02 22:25:46 -0400

    Clean run_scheduler

--- src/scheduler/run_scheduler.py
+++ src/scheduler/run_scheduler.py
@@ -17,56 +17,48 @@ if __name__ == "__main__":
         print "Version should be 0 for mainstream, 1 for nosharing, 2 for maxsharing"
         sys.exit()
 
+    outfile_mainstream = outfile_prefix + "-mainstream"
     outfile_nosharing = outfile_prefix + "-nosharing"
     outfile_maxsharing = outfile_prefix + "-maxsharing"
-    outfile_mainstream = outfile_prefix + "-mainstream"
 
+    params_mainstream = [False, False]
     params_nosharing = [True, False]
     params_maxsharing = [False, True]
-    params_mainstream = [False, False]
 
-    outfiles = [outfile_nosharing, outfile_maxsharing, outfile_mainstream]
-    params_options = [params_nosharing, params_maxsharing, params_mainstream]
+    outfiles = [outfile_mainstream, outfile_nosharing, outfile_maxsharing]
+    params_options = [params_mainstream, params_nosharing, params_maxsharing]
 
     outfile = outfiles[version]
     params = params_options[version]
-
-    correlations = [0, .1664, 1]
 
     num_trials = 3
 
     for i in range(num_trials):
         for outfile, params in zip(outfiles, params_options):
-            if outfile != outfile_mainstream:
-                continue
-            for c in correlations:
-                outfile_full = outfile + "-c" + str(c)
-                print outfile_full
-                with open(outfile_full, "a+", 0) as f:
-                    for num_apps in range(2, num_apps_range+1):
+            with open(outfile, "a+", 0) as f:
+                for num_apps in range(2, num_apps_range+1):
 
-                        # Get Schedule
-                        apps = []
-                        for i in range(1, num_apps + 1):
-                            index = i % len(app_data.app_options)
-                            app = dict(app_data.app_options[index])
-                            app["app_id"] = i
-                            app["correlation"] = c
-                            apps.append(app)
+                    # Get Schedule
+                    apps = []
+                    for i in range(1, num_apps + 1):
+                        index = i % len(app_data.app_options)
+                        app = dict(app_data.app_options[index])
+                        app["app_id"] = i
+                        apps.append(app)
 
-                        s = Scheduler.Scheduler(apps, app_data.video_desc,
-                                                app_data.model_desc, 0)
+                    s = Scheduler.Scheduler(apps, app_data.video_desc,
+                                            app_data.model_desc, 0)
 
-                        metric, cost, avg_rel_acc, num_frozen_list, target_fps_list = s.run(150,
-                                                                                      params[0],
-                                                                                      params[1])
-                        print "Observed FNR:", metric, ", Frozen:", num_frozen_list, \
-                                ", FPS:",  target_fps_list, ", Cost:", cost
+                    metric, cost, avg_rel_acc, num_frozen_list, target_fps_list = s.run(150,
+                                                                                  params[0],
+                                                                                  params[1])
+                    print "Observed FNR:", metric, ", Frozen:", num_frozen_list, \
+                            ", FPS:",  target_fps_list, ", Cost:", cost
 
-                        num_frozen_str = ",".join([str(x) for x in num_frozen_list])
-                        target_fps_str = ",".join([str(x) for x in target_fps_list])
+                    num_frozen_str = ",".join([str(x) for x in num_frozen_list])
+                    target_fps_str = ",".join([str(x) for x in target_fps_list])
 
-                        line = str(num_apps) + "," + str(round(metric, 4)) + "," + \
-                               str(round(avg_rel_acc ,4)) + "," + \
-                               num_frozen_str + "," + target_fps_str + "\n"
-                        f.write(line)
+                    line = str(num_apps) + "," + str(round(metric, 4)) + "," + \
+                           str(round(avg_rel_acc ,4)) + "," + \
+                           num_frozen_str + "," + target_fps_str + "\n"
+                    f.write(line)
