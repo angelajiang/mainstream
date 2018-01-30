186f2c2af3c0e09c1d801b440fe46139fdfc7515
Daniel Wong <wonglkd@gmail.com>
2018-01-30 14:21:38 -0500

    Refactors of inference_h5 for efficiency

--- src/inference/inference_h5.py
+++ src/inference/inference_h5.py
@@ -11,6 +11,39 @@ import tensorflow as tf
 import numpy as np
 
 
+class Model(object):
+    """docstring for Model"""
+    def __init__(self, model_path):
+        super(Model, self).__init__()
+        self.model, self.tags = net.load(model_path)
+        net.compile(self.model)
+        self.dim = 224 if 'mobilenets' in model_path else 299
+        print 'Tags', self.tags
+
+    def predict(self, dataset_dir):
+        data_X, data_y, tags = dataset.dataset(dataset_dir, self.dim, False)
+        print dataset_dir, "shape", data_X.shape
+        if data_X.size == 0:
+            return []
+        return self.model.predict(data_X)
+
+    def predict_by_tag(self, dataset_dir):
+        data_X = dataset.dataset_with_root_dir(dataset_dir, self.dim)
+        tag_index = [i for i, t in enumerate(tags) if t == tag][0]
+        print tags
+
+        predictions = []
+        for d in data_X:
+            X = np.expand_dims(d, axis=0)
+            prediction = (self.model.predict(X)).tolist()[0]
+            argmax = prediction.index(max(prediction))
+            if argmax == tag_index:
+                predictions.append(1)
+            else:
+                predictions.append(0)
+        return predictions
+
+
 def predict_with_dataset(model_path, dataset):
     model, tags = net.load(model_path)
 
@@ -28,10 +61,17 @@ def predict_with_dataset(model_path, dataset):
 def predict(model_path, dataset_dir):
     model, tags = net.load(model_path)
     print tags
-    data_X, data_y, tags = dataset.dataset(dataset_dir, 299, False)
+    # Inception
+    # data_X, data_y, tags = dataset.dataset(dataset_dir, 299, False)
+    # MobileNets
+    data_X, data_y, tags = dataset.dataset(dataset_dir, 224, False)
 
     net.compile(model)
     predictions = []
+    # print 'shape', data_X.shape
+    # prediction = model.predict(data_X)
+    # print prediction.shape
+    # prediction = prediction[:, 0]
     for d in data_X:
         X = np.expand_dims(d, axis=0)
         prediction = model.predict(X)
@@ -41,7 +81,10 @@ def predict(model_path, dataset_dir):
 def predict_by_tag(model_path, dataset_dir, tag):
     model, tags = net.load(model_path)
     tag_index = [i for i, t in enumerate(tags) if t == tag][0]
-    data_X = dataset.dataset_with_root_dir(dataset_dir, 299)
+    # Inception
+    # data_X = dataset.dataset_with_root_dir(dataset_dir, 299)
+    # MobileNets
+    data_X = dataset.dataset_with_root_dir(dataset_dir, 224)
     print tags
 
     net.compile(model)
