63d04be8795b6423d72898983a78d4713cf6adb8
Angela Jiang <to.ajiang@gmail.com>
2018-01-31 21:39:55 -0500

    run.sh

---
+++ run.sh
@@ -0,0 +1,3 @@
+python src/scheduler/run_scheduler_simulator.py 10 1 ~/src/mainstream-analysis/output/streamer/scheduler/atc/f1/f1-train-500
+python src/scheduler/run_scheduler_simulator.py 10 2 ~/src/mainstream-analysis/output/streamer/scheduler/atc/f1/f1-train-500
+python src/scheduler/run_scheduler_simulator.py 10 3 ~/src/mainstream-analysis/output/streamer/scheduler/atc/f1/f1-train-500
