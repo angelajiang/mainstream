120b05f768522afcad73485724b6c73cdd581120
Daniel Wong <wonglkd@gmail.com>
2018-02-02 16:48:24 -0500

    Amendments to previous x-voting implementation

--- src/scheduler/x_voting.py
+++ src/scheduler/x_voting.py
@@ -3,40 +3,36 @@ import random
 def bernoulli(p):
     return int(random.random() < p)
 
-def is_x_detected(acc, correlation, event_length, stride, x_vote, verbose = False):
-
-    conditional_probability = min(acc + correlation, 1)
+def is_x_detected(acc, conditional_probability, event_length, stride, x_vote, verbose = False):
 
     offset = int(stride * random.random()) # don't always sample first frame
     votes = 0
-    previous_detected = None
+    detect = bernoulli(acc)
     for i in range(event_length):
-        if (i + offset) % stride != 0:
+        if (i + offset) % stride != 0:  # Bug: stride is a float, ill-defined
           continue
-        if previous_detected:
-            detect = bernoulli(conditional_probability)
-        else:
-            detect = bernoulli(acc)
         if detect:
             votes += 1
-            if votes is x_vote:
+            if votes >= x_vote:
                 return True
+            detect = bernoulli(conditional_probability)
         else:
             votes = 0
+            detect = bernoulli(acc)
         previous_detected = detect
     return False
 
-def calculate_miss_rate(acc=None,
+def calculate_miss_rate_montecarlo(acc=None,
                         event_length=None,
                         correlation=None,
                         stride=None,
                         x_vote=2,
                         trials=10000):
-    detections = [int(is_x_detected(acc, correlation, int(event_length), stride, x_vote))
+    conditional_probability = min(acc + correlation, 1)
+    detections = [int(is_x_detected(acc, conditional_probability, int(event_length), stride, x_vote))
                     for _ in range(trials)]
     p_detected = sum(detections) / float(trials)
     p_detected_min = 0.00001
     p_detected = max(p_detected, p_detected_min)    # Probability should never be 0
 
     return 1 - p_detected
-
