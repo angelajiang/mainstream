b3642d005d1ec0c6f8844ada73e881dc0b8610c4
Daniel Wong <wonglkd@gmail.com>
2018-02-02 21:31:54 -0500

    Fix cp assert

--- src/scheduler/scheduler_util.py
+++ src/scheduler/scheduler_util.py
@@ -181,7 +181,8 @@ def calculate_miss_rate(p_identified, d, conditional_probability, stride):
 # d: length of event to hit/miss in number of frames
 
     conditional_probability_miss = 1 - conditional_probability
-    assert conditional_probability_miss < conditional_probability
+    if conditional_probability_miss < 1 - p_identified:
+        warnings.warn("{} < {}".format(conditional_probability_miss, 1 - p_identified), stacklevel=2)
 
     d = float(d)
     stride = float(stride)
