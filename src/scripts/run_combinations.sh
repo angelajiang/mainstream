486fcde7f628b3ccbf2d7f0797e8d8472469ad53
Daniel Wong <wonglkd@gmail.com>
2018-02-04 18:07:09 -0500

    Add cat at end of run_combinations

--- src/scripts/run_combinations.sh
+++ src/scripts/run_combinations.sh
@@ -17,3 +17,4 @@ for i in $(seq 1 $MAX_NUM_APPS); do
     $SEM_CMD1 python src/scheduler/run_scheduler_simulator.py $i ../mainstream-analysis/output/streamer/scheduler/atc/$OPTIMIZE_METRIC/$OPTIMIZE_METRIC-combinations-$i --combs --combs-max-samples $LIMIT_MAX_SAMPLES --metric $OPTIMIZE_METRIC --x-vote $X_VOTING --datasets $DATASETS
 done
 $SEM_CMD2
+cat ../mainstream-analysis/output/streamer/scheduler/atc/$OPTIMIZE_METRIC/$OPTIMIZE_METRIC-combinations-*-mainstream-simulator > ../mainstream-analysis/output/streamer/scheduler/atc/$OPTIMIZE_METRIC/$OPTIMIZE_METRIC-combo-mainstream-simulator
