965dc29d29968a9f62b0977d7dfd90fcc6643103
Daniel Wong <wonglkd@gmail.com>
2018-02-04 00:36:34 -0500

    Run_combinations

--- src/scripts/run_combinations.sh
+++ src/scripts/run_combinations.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-MAX_NUM_APPS=20
+MAX_NUM_APPS=30
 OPTIMIZE_METRIC=f1
-LIMIT_MAX_SAMPLES=20
+LIMIT_MAX_SAMPLES=40
 X_VOTING=0
 for i in $(seq 1 $MAX_NUM_APPS); do
     sem -j+0 python src/scheduler/run_scheduler_simulator.py $i ../mainstream-analysis/output/streamer/scheduler/atc/$OPTIMIZE_METRIC/$OPTIMIZE_METRIC-combinations-$i --combs --combs-max-samples $LIMIT_MAX_SAMPLES --metric $OPTIMIZE_METRIC --x-vote $X_VOTING
