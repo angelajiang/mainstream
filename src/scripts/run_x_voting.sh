c61d9e10fd65c01e6dda266e8290e7af611c7ac6
Daniel Wong <wonglkd@gmail.com>
2018-02-05 04:49:15 -0500

    Working run_x_voting.sh

--- src/scripts/run_x_voting.sh
+++ src/scripts/run_x_voting.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 MAX_NUM_APPS=30
-DATASET=trains
-SEM_CMD1=sem -j+0 
-SEM_CMD2=sem --wait
-if [[ ! type sem > /dev/null ]]; then
-    echo "sem (part of GNU parallel not installed)"
-    echo "Try 'pkg install parallel'"
-    echo "processing without it"
-    SEM_CMD1=
-    SEM_CMD2=
-fi
-for i in 0 1 2 3; do
-    for j in f1 fnr fpr; do
-        $SEM_CMD1 python src/scheduler/run_scheduler_simulator.py $MAX_NUM_APPS ../mainstream-analysis/output/streamer/scheduler/atc/$j/$j-$DATASET-500 -m $j --x-vote $i --datasets $DATASET
+#SEM_CMD1=sem -j+0
+#SEM_CMD2=sem --wait
+
+#if [[ ! type sem > /dev/null ]]; then
+#    echo "sem (part of GNU parallel not installed)"
+#    echo "Try 'pkg install parallel'"
+#    echo "processing without it"
+#    SEM_CMD1=
+#    SEM_CMD2=
+#fi
+#    for j in f1 fnr fpr; do
+for j in f1; do
+    for DATASET in pedestrian train; do
+        for i in 1 2 3 4 5 6 7; do
+            sem -j+0 python src/scheduler/run_scheduler_simulator.py $MAX_NUM_APPS ../mainstream-analysis/output/streamer/scheduler/atc/$j/$j-$DATASET-500 -m $j --x-vote $i --datasets $DATASET > log/atc/$j-$DATASET-500-x$i-mainstream-simulator.out
+        done
+        # sem -j+0 python src/scheduler/run_scheduler_simulator.py $MAX_NUM_APPS ../mainstream-analysis/output/streamer/scheduler/atc/$j/$j-$DATASET-500 -m $j --datasets $DATASET > log/atc/$j-$DATASET-500-mainstream-simulator.out
     done
 done
-$SEM_CMD2
+sem --wait
+#$SEM_CMD2
