e882057285f97e9a7259004d59d96a56fa5428c0
Angela Jiang <to.ajiang@gmail.com>
2018-02-04 21:14:15 -0500

    Run no+max sharing with combinations

--- src/scripts/run_nuc.sh
+++ src/scripts/run_nuc.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
-MAX_NUM_APPS=30
-DATASET=train
+MAX_NUM_APPS=4
+OPTIMIZE_METRIC=f1
+LIMIT_MAX_SAMPLES=100
+DATASETS="pedestrian cars flowers cats"
+#DATASET=train
+
+
 for metric in f1; do
-    python src/scheduler/run_scheduler.py $MAX_NUM_APPS ../mainstream-analysis/output/streamer/scheduler/atc/$metric/$metric-$DATASET -m $metric --versions nosharing maxsharing --trials 1 --datasets $DATASET
+    prefix="../mainstream-analysis/output/streamer/scheduler/atc/$metric/combos/$metric-4hybrid-combo"
+    for i in $(seq 1 $MAX_NUM_APPS); do
+        python src/scheduler/run_scheduler.py $MAX_NUM_APPS \
+                                              $prefix"-numapps-"$i\
+                                              -m $metric \
+                                              --versions nosharing maxsharing \
+                                              --trials 1 \
+                                              --combs \
+                                              --combs-max-samples $LIMIT_MAX_SAMPLES \
+                                              --datasets $DATASETS
+    done
+    cat $prefix*"numapps"* > $prefix"-all-"$MAX_NUM_APPS
 done
+
+
