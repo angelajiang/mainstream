649e6e2c01a886b55941e6d26da30bf652f66555
Daniel Wong <wonglkd@gmail.com>
2018-02-06 18:12:00 -0500

    Fix scripts

--- src/scripts/run_ms-sim.sh
+++ src/scripts/run_ms-sim.sh
@@ -2,13 +2,11 @@
 MAX_NUM_APPS=32
 DELTA=1
 for metric in f1; do
-    prefix="../mainstream-analysis/output/streamer/scheduler/atc/$metric/$metric-4hybrid-real"
     for dataset in train pedestrian; do
-        for i in $(seq $DELTA $MAX_NUM_APPS); do
-            python src/scheduler/run_scheduler_simulator.py $i \
-                                                            $prefix \
-                                                            -m $metric \
-                                                            --datasets $dataset
-        done
+        prefix="../mainstream-analysis/output/streamer/scheduler/atc/$metric/$metric-$dataset-500-real"
+        python src/scheduler/run_scheduler_simulator.py $MAX_NUM_APPS \
+                                                        $prefix \
+                                                        -m $metric \
+                                                        --datasets $dataset
     done
 done
--- src/scripts/run_ms-sim_4hybrid.sh
+++ src/scripts/run_ms-sim_4hybrid.sh
@@ -3,11 +3,9 @@ MAX_NUM_APPS=32
 DELTA=4
 DATASET="pedestrian flowers cars cats"
 for metric in f1; do
-    prefix="../mainstream-analysis/output/streamer/scheduler/atc/$metric/$metric-4hybrid-real"
-    for i in $(seq $DELTA $MAX_NUM_APPS); do
-        python src/scheduler/run_scheduler_simulator.py $i \
-                                                        $prefix \
-                                                        -m $metric \
-                                                        --datasets $DATASET
-    done
+    prefix="../mainstream-analysis/output/streamer/scheduler/atc/$metric/$metric-4hybrid-real2"
+    python src/scheduler/run_scheduler_simulator.py $MAX_NUM_APPS \
+                                                    $prefix \
+                                                    -m $metric \
+                                                    --datasets $DATASET
 done
