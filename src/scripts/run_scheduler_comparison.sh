694393326a7ec4763dd26bf6cb3ee750f3cf6df8
Angela Jiang <to.ajiang@gmail.com>
2018-03-14 16:16:12 -0400

    Prepare 031418 experiment 200 setups per num apps 432 possible setups

--- src/scripts/run_scheduler_comparison.sh
+++ src/scripts/run_scheduler_comparison.sh
@@ -1,10 +1,10 @@
 DATA_DIR="data/cpp/"
-RUN_ID="031318.v0"
+RUN_ID="031418.v0"
 VERBOSE=0
 NUM_APPS_RANGE=30
-NUM_SETUPS=50
+NUM_SETUPS=200
 STREAM_FPS=10
-SETUP_CONFIG="config/scheduler/031318.v0"
+SETUP_CONFIG="config/scheduler/031418.v0"
 SETUPS_FILE=$DATA_DIR"/setups."$RUN_ID
 SCHEDULER_TYPE="greedy"
 
@@ -23,7 +23,7 @@ python src/scheduler/exhaustive_search.py -v $VERBOSE \
                                           -t $SCHEDULER_TYPE \
                                           -n $NUM_APPS_RANGE
 
-g++ -std=c++0x  src/scheduler/cpp/exhaustive_search.cpp \
-                src/scheduler/cpp/schedule.cpp \
-                src/scheduler/cpp/schedule_unit.cpp \
-                && ./a.out $DATA_DIR $RUN_ID
+#g++ -std=c++0x  src/scheduler/cpp/exhaustive_search.cpp \
+#                src/scheduler/cpp/schedule.cpp \
+#                src/scheduler/cpp/schedule_unit.cpp \
+#                && ./a.out $DATA_DIR $RUN_ID
