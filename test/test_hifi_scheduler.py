e203cd614acf5b1d558af40a7117e836c598b242
Angela Jiang <to.ajiang@gmail.com>
2018-03-20 21:25:27 -0400

    Add hifi vs exhaustive test that fails

---
+++ test/test_hifi_scheduler.py
@@ -0,0 +1,32 @@
+import os
+import subprocess
+import pytest
+
+@pytest.mark.unit
+def test_hifi_scheduler():
+    output_dir = os.path.join("test", "tmp")
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    schedules_dir = os.path.join(output_dir, "schedules")
+    hifi_file = os.path.join(schedules_dir, "hifi.sim.debug.v0")
+    exhaustive_file = os.path.join(schedules_dir, "exhaustive.sim.debug.v0")
+
+    subprocess.call("test/hifi_vs_exhaustive.sh", shell=True)
+
+    hifi_f1s = []
+    with open(hifi_file, "r") as f:
+        for line in f:
+            vals = line.split(',')
+            hifi_f1s.append(round(vals[1], 2))
+
+    exhaustive_f1s = []
+    with open(exhaustive_file, "r") as f:
+        for line in f:
+            vals = line.split(',')
+            exhaustive_f1s.append(round(vals[1], 2))
+
+    assert hifi_f1s == exhaustive_f1s
+
+    os.rmdir(output_dir)
+
