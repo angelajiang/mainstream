260fb1024007cea01f27d67c07cab1c28f71eca5
Angela Jiang <to.ajiang@gmail.com>
2018-03-24 14:11:32 -0400

    Add performance test for exhaustive search

---
+++ src/scripts/test_exhaustive_search.sh
@@ -0,0 +1,23 @@
+DATA_DIR="data/cpp"
+RUN_ID="latency.v0"
+VERBOSE=0
+NUM_APPS_RANGE=10
+NUM_SETUPS=1
+STREAM_FPS=1
+SETUP_CONFIG="config/scheduler/setup.v0"
+SETUPS_FILE=$DATA_DIR"/setups."$RUN_ID
+SCHEDULER_TYPE="hifi"
+SIMULATOR=1
+
+# Only need to generate this once
+python src/scheduler/generate_setups.py -r $RUN_ID \
+                                        -n $NUM_APPS_RANGE \
+                                        -o $DATA_DIR \
+                                        -s $NUM_SETUPS \
+                                        -f $STREAM_FPS \
+                                        -c $SETUP_CONFIG
+
+g++ -std=c++0x  src/scheduler/cpp/exhaustive_search.cpp \
+                src/scheduler/cpp/schedule.cpp \
+                src/scheduler/cpp/schedule_unit.cpp \
+                && ./a.out $DATA_DIR $RUN_ID
