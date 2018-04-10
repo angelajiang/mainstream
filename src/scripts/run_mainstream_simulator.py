bad922657803cb6740c69ab0434106d70fb9a834
Angela Jiang <to.ajiang@gmail.com>
2018-04-09 22:21:09 -0400

    Add budget and scheduler to script

--- src/scripts/run_mainstream_simulator.py
+++ src/scripts/run_mainstream_simulator.py
@@ -1,13 +1,13 @@
 #!/bin/bash
-MAX_NUM_APPS=4
-DATASET="pedestrian flowers cars cats"
+MAX_NUM_APPS=3
+DATASET="flowers cars cats"
 SCHEDULER="hifi"
-BUDGET=50
+BUDGET=350
 mode="f1"
 for mode in mainstream nosharing maxsharing; do
     for i in $(seq 1 $MAX_NUM_APPS); do
         python src/scheduler/run_scheduler_simulator.py \
-                $i ../mainstream-analysis/output/streamer/scheduler/debug/$metric-$mode \
+                $i ../mainstream-analysis/output/streamer/scheduler/debug/scheduler$metric-$mode \
                 --mode $mode \
                 --datasets $DATASET \
                 --budget $BUDGET \
