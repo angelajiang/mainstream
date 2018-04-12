a75a9315ef55ee96f5ab832225c9c3115ac625f6
Angela Jiang <to.ajiang@gmail.com>
2018-04-11 22:39:56 -0400

    Bug fix in tests

--- test/test_optimal_schedulers.py
+++ test/test_optimal_schedulers.py
@@ -10,6 +10,7 @@ def test_optimality(scheduler_type):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
+    schedules_dir = os.path.join(output_dir, "schedules")
     if not os.path.exists(schedules_dir):
         os.makedirs(schedules_dir)
     other_file = os.path.join(schedules_dir, scheduler_type + ".mainstream.sim.debug.v0")
