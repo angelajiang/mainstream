35fbf50bd28ac0a0db331c0b782e8e430755df46
Angela Jiang <to.ajiang@gmail.com>
2018-04-11 22:30:42 -0400

    Add intial osdi scripts

---
+++ src/scripts/osdi/run_all_modes_with_setups.sh
@@ -0,0 +1,30 @@
+DATA_DIR="data/cpp/"
+RUN_ID="041118.v0"
+VERBOSE=0
+NUM_APPS_RANGE=16
+NUM_SETUPS=10
+STREAM_FPS=15
+SETUP_CONFIG="config/scheduler/setup.v0"
+SETUPS_FILE=$DATA_DIR"/setups."$RUN_ID
+SCHEDULER_TYPE="greedy"
+SIMULATOR=1
+
+# Only need to generate this once
+python src/scheduler/generate_setups.py -r $RUN_ID \
+                                        -n $NUM_APPS_RANGE \
+                                        -o $DATA_DIR \
+                                        -s $NUM_SETUPS \
+                                        -f $STREAM_FPS \
+                                        -c $SETUP_CONFIG
+
+for MODE in maxsharing mainstream nosharing
+do
+python src/scheduler/run_scheduler_with_setups.py -v $VERBOSE \
+                                                  -o $DATA_DIR \
+                                                  -r $RUN_ID \
+                                                  -f $SETUPS_FILE \
+                                                  -t $SCHEDULER_TYPE \
+                                                  -s $SIMULATOR \
+                                                  --mode $MODE
+done
+
