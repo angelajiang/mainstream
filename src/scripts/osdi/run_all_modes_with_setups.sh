a8385dac49c2e918dafecf4b186a33f073e83436
Angela Jiang <to.ajiang@gmail.com>
2018-04-16 20:31:36 -0400

    Experiment 041618 setup

--- src/scripts/osdi/run_all_modes_with_setups.sh
+++ src/scripts/osdi/run_all_modes_with_setups.sh
@@ -1,21 +1,23 @@
 DATA_DIR="data/cpp/"
-RUN_ID="041118.v0"
+RUN_ID="041618.v0"
 VERBOSE=0
-NUM_APPS_RANGE=15
-NUM_SETUPS=10
+NUM_APPS=10
+NUM_SETUPS=20
 STREAM_FPS=10
 SETUP_CONFIG="config/scheduler/setup.v0"
 SETUPS_FILE=$DATA_DIR"/setups."$RUN_ID
 SCHEDULER_TYPE="greedy"
+SWEEP=0
 SIMULATOR=1
 
 # Only need to generate this once
 python src/scheduler/generate_setups.py -r $RUN_ID \
-                                        -n $NUM_APPS_RANGE \
+                                        -n $NUM_APPS \
                                         -o $DATA_DIR \
                                         -s $NUM_SETUPS \
                                         -f $STREAM_FPS \
-                                        -c $SETUP_CONFIG
+                                        -c $SETUP_CONFIG \
+                                        -sn $SWEEP
 
 for MODE in maxsharing mainstream nosharing
 do
