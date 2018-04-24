832b5e94d337367bf7e3324b1c5adba00af86225
Angela Jiang <to.ajiang@gmail.com>
2018-04-24 17:48:42 -0400

    Fix test arguments

--- test/run_scheduler_with_setups.sh
+++ test/run_scheduler_with_setups.sh
@@ -43,6 +43,5 @@ else
                                                       -r $RUN_ID \
                                                       -f $SETUPS_FILE \
                                                       -t $SCHEDULER_TYPE \
-                                                      -s $SIMULATOR \
-                                                      -n $NUM_APPS_RANGE
+                                                      -s $SIMULATOR
 fi
