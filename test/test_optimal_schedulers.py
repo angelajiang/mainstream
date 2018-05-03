9fb65b8ab2ae9cdb0b48a48b064bbfe951a56c7c
Daniel Wong <wonglkd@gmail.com>
2018-05-03 16:16:40 -0400

    Refactor tests

--- test/test_optimal_schedulers.py
+++ test/test_optimal_schedulers.py
@@ -15,37 +15,39 @@ def get_dir(idx):
 
 
 @pytest.mark.unit
-@pytest.mark.parametrize("scheduler_type", ["hifi", "stems", "stems_cpp"])
-def test_optimality(scheduler_type):
-    idx = "exhaustive" + "_vs_" + scheduler_type
+@pytest.mark.parametrize("scheduler_ref,scheduler_test,num_apps,check_schedules", [
+    ("exhaustive", ["hifi", "stems", "stems_cpp"], 3, False),
+])
+def test_optimality(scheduler_ref, schedulers_test, num_apps, check_schedules):
+    idx = scheduler_ref + "_vs_" + ",".join(schedulers_test)
     output_dir, schedules_dir = get_dir(idx)
-    other_file = os.path.join(schedules_dir, scheduler_type + ".mainstream.sim.100.debug.v1")
-    exhaustive_file = os.path.join(schedules_dir, "exhaustive.mainstream.sim.100.debug.v1")
-
     subprocess.check_call("test/gen_setups.sh {} {} {} {}".format(idx,
-                                                                  3,
+                                                                  num_apps,
                                                                   output_dir,
                                                                   "config/scheduler/setup_fast.v1"), shell=True)
-    subprocess.check_call("test/run_scheduler_only.sh {} {}".format("exhaustive",
-                                                                    output_dir), shell=True)
-    subprocess.check_call("test/run_scheduler_only.sh {} {}".format(scheduler_type,
-                                                                    output_dir), shell=True)
 
-    other_f1s = []
-    with open(other_file, "r") as f:
-        for line in f:
-            vals = line.split(',')
-            metric = round(float(vals[1]), 2)
-            other_f1s.append(metric)
+    all_f1s = {}
+    all_schedules = {}
 
-    exhaustive_f1s = []
-    with open(exhaustive_file, "r") as f:
-        for line in f:
-            vals = line.split(',')
-            metric = round(float(vals[1]), 2)
-            exhaustive_f1s.append(metric)
+    for scheduler in [scheduler_ref] + schedulers_test:
+        subprocess.check_call("test/run_scheduler_only.sh {} {}".format(scheduler,
+                                                                        output_dir), shell=True)
+        f1s = []
+        schedules = []
+        filename = os.path.join(schedules_dir, scheduler + ".mainstream.sim.100.debug.v1")
+        with open(filename, "r") as f:
+            for line in f:
+                vals = line.split(',')
+                f1s.append(round(float(vals[2]), 3))
+                num_apps = int(vals[1])
+                schedules.append(vals[3:3 + num_apps * 2])
+        all_f1s[scheduler] = f1s
+        all_schedules[scheduler] = schedules
 
-    assert other_f1s == exhaustive_f1s
+    for scheduler in schedulers_test:
+        assert all_f1s[scheduler] == all_f1s[scheduler_ref]
+        if check_schedules:
+            assert all_schedules[scheduler] == all_schedules[scheduler_ref]
 
     shutil.rmtree(output_dir)
 
