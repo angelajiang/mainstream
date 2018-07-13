9845b68b6cac7f0c57733da1fa6fe5f137331a62
Daniel Lin-Kit Wong <liliatangxy@gmail.com>
2018-07-13 11:18:42 -0400

    testing script

--- test/run_inference_manually.sh
+++ test/run_inference_manually.sh
@@ -1,4 +1,4 @@
-python src/training/train.py config/test/1-epoch ~/image-data/training/train_images_small/ ~/models/test-model 310
-python src/inference/freeze.py ~/models/test-model
-python src/inference/inference_h5.py ~/models/test-model ~/image-data/training/train_images_small/ | tee log/h5-out
-python src/inference/inference_pb.py ~/models/test-model-frozen.pb ~/image-data/training/train_images_small/ | tee log/pb-out
+python src/training/train.py config/test/1-epoch /datasets/BigLearning/ahjiang/image-data/training/pedestrian ~/atrium/0 84
+python src/inference/freeze.py ~/atrium/0
+python src/inference/inference_h5.py ~/atrium/0 /datasets/BigLearning/ahjiang/image-data/test/pedestrian | tee log/h5-out
+python src/inference/inference_pb.py ~/atrium/0/atrium-mobilenets-0.pb ~/image-data/test/pedestrian | tee log/pb-out
